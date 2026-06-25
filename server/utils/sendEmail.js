b15ac05f636b90cdd916027d2bfba6370de110cf
Eimanson Maybituin <ecmabta8@gmail.com>
2026-06-25 11:32:47 +0800

    Install Resend

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -3,9 +3,9 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  family: 4,
+  port: 465,
+  secure: true,
+  // family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
